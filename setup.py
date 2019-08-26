e6a31af9207f9674a488f87d87d5fcddb7104866
Jack <john.harmon96@gmail.com>
2019-08-25 21:01:36 -0400

    Standard Function Repo for Unanet

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='jackLib',
+	version='1.0',
+	description='Unanet Module',
+	author='Jack Harmon',
+	author_email='john.harmon96@gmail.com',
+	packages=['jackLib'],
+	install_requires=['selenium']
+)
